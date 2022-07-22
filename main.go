6fd5e1f02297dfa8574d8dad3f63678417ef1f87
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2022-07-22 15:45:32 +0000

    Minor refactor to the main daemon

--- main.go
+++ main.go
@@ -8,32 +8,33 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func daemon(app *pocketbase.PocketBase) {
+	syncer, err := syncer.NewSyncer(app.Dao(), "circuit.rocks")
+	if err != nil {
+		log.Fatal("intantiate syncer: %v", err)
+	}
+
+	if err := syncer.CollectAllItems(); err != nil {
+		log.Fatal("collect all live tenant items: %v", err)
+	}
+
+	intentItems, err := syncer.IntentTenant.CollectAllItems()
+	if err != nil {
+		log.Fatalf("collect all intent items: %v", err)
+	}
+	for _, item := range intentItems {
+		err := syncer.SyncItem(item.SellerSKU)
+		if err != nil {
+			log.Fatalf("syncing %q: %v", item.SellerSKU, err)
+		}
+	}
+}
+
 func main() {
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		go func(app *pocketbase.PocketBase) {
-			syncer, err := syncer.NewSyncer(app.Dao(), "circuit.rocks")
-			if err != nil {
-				log.Fatal("intantiate syncer: %v", err)
-			}
-
-			if err := syncer.CollectAllItems(); err != nil {
-				log.Fatal("collect all live tenant items: %v", err)
-			}
-
-			intentItems, err := syncer.IntentTenant.CollectAllItems()
-			if err != nil {
-				log.Fatalf("collect all intent items: %v", err)
-			}
-			for _, item := range intentItems {
-				err := syncer.SyncItem(item.SellerSKU)
-				if err != nil {
-					log.Fatalf("syncing %q: %v", item.SellerSKU, err)
-				}
-			}
-		}(app)
-
+		go daemon(app)
 		return nil
 	})
 
