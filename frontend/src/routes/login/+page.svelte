1acee6a5661de50b3168ab54ff132c288fdc3b33
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2022-10-16 12:57:39 +0000

    fix(styles): Fix tailwindcss scope string.

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,10 +1,24 @@
-<form class="login-form">
-    <input type="text" />
-    <input type="password" />
-</form>
+<div class="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+    <h1 class="text-3xl font-semibold text-center text-gray-700 dark:text-white">sellersync.io</h1>
 
-<style lang="scss">
-    .login-form {
-        background-color: black;
-    }
-</style>
+    <form class="mt-6">
+        <div>
+            <label for="username" class="block text-sm text-gray-800 dark:text-gray-200">Username</label>
+            <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+        </div>
+
+        <div class="mt-4">
+            <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Password</label>
+            </div>
+
+            <input type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+        </div>
+
+        <div class="mt-6">
+            <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                Login
+            </button>
+        </div>
+    </form>
+</div>
